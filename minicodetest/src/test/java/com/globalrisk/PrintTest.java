815b949c29f297e816c2764334f23c15f4a98a9d
psh <544341796@qq.com>
2019-03-11 19:42:10 +0800

    first commit

---
+++ minicodetest/src/test/java/com/globalrisk/PrintTest.java
@@ -0,0 +1,18 @@
+package com.globalrisk;
+
+import org.junit.Test;
+
+/**
+ * Unit test for Mini Code Test
+ */
+public class PrintTest{
+    /**
+     * print Test
+     */
+	@Test
+    public void testPrint(){
+	   PrintFizzBuzz printFizzBuzz=new PrintFizzBuzz();
+	   printFizzBuzz.print();
+    }
+    
+}
